d3ddc88a53a89ee8209c1ea3736d20025e768a64
zmakhkha <jcaladic@gmail.com>
2024-10-18 10:01:17 +0100

    feat : added logout functionality

--- front/src/app/components/main/Sidebar.tsx
+++ front/src/app/components/main/Sidebar.tsx
@@ -1,98 +1,156 @@
 "use client";
-import React from "react";
-import { Flex, IconButton, Box, Tooltip, Link } from "@chakra-ui/react";
+import React, { useRef } from "react";
+import {
+  Flex,
+  IconButton,
+  Box,
+  Tooltip,
+  Link,
+  useDisclosure,
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+  Button,
+} from "@chakra-ui/react";
 import { FaHome, FaChartLine, FaLeaf, FaCog } from "react-icons/fa";
 import { PiSigmaBold } from "react-icons/pi";
+import { IoLogOut } from "react-icons/io5";
 import useColorModeStyles from "@/app/utils/useColorModeStyles";
+import { useRouter } from "next/navigation";
 
 const Sidebar = () => {
-  const { bg, hoverColor } = useColorModeStyles(); // Get styles from the utility
+  const { bg, hoverColor } = useColorModeStyles();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = useRef(null);
+  const router = useRouter();
+
+  const handleLogout = () => {
+    localStorage.clear();
+    onClose();
+    router.push("/login");
+  };
 
   return (
-    <Flex
-      direction="column"
-      align="center"
-      bg={bg}
-      p={4}
-      width="100%"
-      height="100%"
-    >
-      {/* Home Icon */}
-      <Tooltip label="Dashboard" aria-label="Home">
-        <Link href="/dashboard">
+    <>
+      <Flex
+        direction="column"
+        align="center"
+        bg={bg}
+        p={4}
+        width="100%"
+        height="100%"
+      >
+        <Tooltip label="Dashboard" aria-label="Home">
+          <Link href="/dashboard">
+            <IconButton
+              icon={<FaHome />}
+              aria-label="Home"
+              variant="ghost"
+              mb={2}
+              _hover={{ color: hoverColor }}
+            />
+          </Link>
+        </Tooltip>
+
+        <Box height="1px" width="20px" bg="gray.400" mb={2} />
+
+        <Tooltip label="Analytique" aria-label="Analytics">
+          <Link href="/analytics">
+            <IconButton
+              icon={<FaChartLine />}
+              aria-label="Analytics"
+              variant="ghost"
+              mb={2}
+              _hover={{ color: hoverColor }}
+            />
+          </Link>
+        </Tooltip>
+
+        <Box height="1px" width="20px" bg="gray.400" mb={2} />
+
+        <Tooltip label="Station météo" aria-label="Sigma">
+          <Link href="/station">
+            <IconButton
+              icon={<PiSigmaBold />}
+              aria-label="Sigma"
+              variant="ghost"
+              mb={2}
+              _hover={{ color: hoverColor }}
+            />
+          </Link>
+        </Tooltip>
+
+        <Box height="1px" width="20px" bg="gray.400" mb={2} />
+
+        <Tooltip label="Humidité du sol" aria-label="Soil Moisture">
+          <Link href="/moisture">
+            <IconButton
+              icon={<FaLeaf />}
+              aria-label="Soil Moisture"
+              variant="ghost"
+              mb={2}
+              _hover={{ color: hoverColor }}
+            />
+          </Link>
+        </Tooltip>
+
+        <Box height="1px" width="20px" bg="gray.400" mb={2} />
+
+        <Tooltip label="Paramètres" aria-label="Settings">
+          <Link href="/settings">
+            <IconButton
+              icon={<FaCog />}
+              aria-label="Settings"
+              variant="ghost"
+              _hover={{ color: hoverColor }}
+            />
+          </Link>
+        </Tooltip>
+
+        <Box height="1px" width="20px" bg="gray.400" mb={2} />
+
+        <Tooltip label="Se déconnecter" aria-label="Logout">
           <IconButton
-            icon={<FaHome />}
-            aria-label="Home"
+            icon={<IoLogOut style={{ transform: "scaleX(-1)" }} />} // Reverse the icon
+            aria-label="Logout"
             variant="ghost"
-            mb={2}
+            onClick={onOpen}
             _hover={{ color: hoverColor }}
           />
-        </Link>
-      </Tooltip>
+        </Tooltip>
+      </Flex>
 
-      {/* Delimiter */}
-      <Box height="1px" width="20px" bg="gray.400" mb={2} />
+     
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Confirmer la déconnexion
+            </AlertDialogHeader>
 
-      {/* Analytics Icon */}
-      <Tooltip label="analytique" aria-label="Analytics">
-        <Link href="/analytics">
-          <IconButton
-            icon={<FaChartLine />}
-            aria-label="Analytics"
-            variant="ghost"
-            mb={2}
-            _hover={{ color: hoverColor }}
-          />
-        </Link>
-      </Tooltip>
+            <AlertDialogBody>
+              Êtes-vous sûr de vouloir vous déconnecter ?
+            </AlertDialogBody>
 
-      {/* Delimiter */}
-      <Box height="1px" width="20px" bg="gray.400" mb={2} />
-
-      {/* Sigma Icon */}
-      <Tooltip label="Station météo" aria-label="Sigma">
-        <Link href="/station">
-          <IconButton
-            icon={<PiSigmaBold />}
-            aria-label="Sigma"
-            variant="ghost"
-            mb={2}
-            _hover={{ color: hoverColor }}
-          />
-        </Link>
-      </Tooltip>
-
-      {/* Delimiter */}
-      <Box height="1px" width="20px" bg="gray.400" mb={2} />
-
-      {/* Soil Moisture Icon */}
-      <Tooltip label="Humidité du sol" aria-label="Soil Moisture">
-        <Link href="/moisture">
-          <IconButton
-            icon={<FaLeaf />}
-            aria-label="Soil Moisture"
-            variant="ghost"
-            mb={2}
-            _hover={{ color: hoverColor }}
-          />
-        </Link>
-      </Tooltip>
-
-      {/* Delimiter */}
-      <Box height="1px" width="20px" bg="gray.400" mb={2} />
-
-      {/* Settings Icon */}
-      <Tooltip label="Paramètres" aria-label="Settings">
-        <Link href="/settings">
-          <IconButton
-            icon={<FaCog />}
-            aria-label="Settings"
-            variant="ghost"
-            _hover={{ color: hoverColor }}
-          />
-        </Link>
-      </Tooltip>
-    </Flex>
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                Annuler
+              </Button>
+              <Button colorScheme="red" onClick={handleLogout} ml={3}>
+                Se déconnecter
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </>
   );
 };
 
