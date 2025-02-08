4b7b0584b86b2afa97cee67253383536e31cb479
zakaria <jcaladic@gmail.com>
2025-02-08 23:24:05 +0100

    server working well

---
+++ a.py
@@ -0,0 +1,3 @@
+curl.exe -X POST http://localhost:9090 `
+     -H "Content-Type: application/json" `
+     -d "{""client"": ""Router04"", ""ec_soil_medium"": 185, ""ec_soil_low"": 185, ""soil_moisture_low"": 18.5, ""soil_temperature_low"": 18.5, ""soil_moisture_medium"": 18.6, ""soil_temperature_high"": 18.5, ""soil_ec_high"": 0, ""soil_moisture_high"": 0.0, ""timestamp"": ""2025-02-06T07:04:46.129594""}"
