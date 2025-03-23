f90e1c443460a17c0d359788605e2655cc12ea01
zmakhkha <jcaladic@gmail.com>
2025-03-23 23:23:31 +0000

    the docker arch is working fine

--- back/agriBack/settings.py
+++ back/agriBack/settings.py
@@ -1,29 +1,33 @@
 from datetime import timedelta
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Load environment variables from .env file
+load_dotenv()
 
-import environ
+# === DJANGO CONFIG ===
+SECRET_KEY = os.getenv('SECRET_KEY', '+@*@loo#%*ay6*m8w1xy7)l2+$iueppj)ns(nj0r6^@+@ujokd')
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-# Initialize environment variables
-env = environ.Env()
-# Reading .env file
-environ.Env.read_env()
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,0.0.0.0,192.168.1.184,192.168.1.184:3000').split(',')
 
-# DJANGO Config
-SECRET_KEY = env('SECRET_KEY', default='+@*@loo#%*ay6*m8w1xy7)l2+$iueppj)ns(nj0r6^@+@ujokd')
-DEBUG = env.bool('DEBUG', default=True)
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1', '0.0.0.0', '192.168.1.184', '192.168.1.184:3000'])
+# === CORS CONFIG ===
+CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', 'http://localhost:3000').split(',')
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 
-# CORS Config
-CORS_ALLOWED_ORIGINS = env.list('CORS_ALLOWED_ORIGINS', default=['http://localhost:3000'])
+# === CSRF CONFIG ===
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', 'http://localhost:3000,http://192.168.1.1:3000').split(',')
+CSRF_COOKIE_HTTPONLY = False
+CSRF_COOKIE_SECURE = False
+CSRF_COOKIE_SAMESITE = "Lax"
+SESSION_COOKIE_SAMESITE = "Lax"
 
-
-
-# Application definition
-
+# === APPLICATIONS ===
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,6 +42,7 @@ INSTALLED_APPS = [
     'CustomUser',
 ]
 
+# === MIDDLEWARE ===
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
@@ -51,6 +56,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'agriBack.urls'
 
+# === TEMPLATES ===
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -69,93 +75,52 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'agriBack.wsgi.application'
 
-# sqlite
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# === DATABASE CONFIG ===
+USE_POSTGRES = os.getenv('USE_POSTGRES', 'True') == 'True'
+
+if USE_POSTGRES:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('POSTGRES_DB', 'agrydata_db'),
+            'USER': os.getenv('POSTGRES_USER', 'agry_admin'),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'Str0ngP@ssw0rd!'),
+            'HOST': os.getenv('POSTGRES_HOST', 'agrydata'),
+            'PORT': int(os.getenv('POSTGRES_PORT', 5432)),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
-# postgres
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': env('POSTGRES_DB', default='agrydata_db'),
-#         'USER': env('POSTGRES_USER', default='agry_admin'),
-#         'PASSWORD': env('POSTGRES_PASSWORD', default='Str0ngP@ssw0rd!'),
-#         'HOST': env('POSTGRES_HOST', default='agrydata'),
-#         'PORT': env('POSTGRES_PORT', default='5432'),
-#     }
-# }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': env('POSTGRES_DB', default='agrydata_db'),
-#         'USER': env('POSTGRES_USER', default='agry_admin'),
-#         'PASSWORD': env('POSTGRES_PASSWORD', default='Str0ngP@ssw0rd!'),
-#         'HOST': env('POSTGRES_HOST', default='agrydata'),
-#         'PORT': env('POSTGRES_PORT', default='5432'),
-#     }
-# }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-
+# === PASSWORD VALIDATORS ===
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
-
+# === INTERNATIONALIZATION ===
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+# === STATIC FILES ===
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
+# === PRIMARY KEY ===
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_CREDENTIALS = True
 
-
-
+# === AUTH USER MODEL ===
 AUTH_USER_MODEL = 'CustomUser.CustomUser'
 
-# REST_FRAMEWORK = {
-#     'DEFAULT_AUTHENTICATION_CLASSES': (
-#         'rest_framework_simplejwt.authentication.JWTAuthentication',
-#     ),
-# }
-
-
+# === REST FRAMEWORK ===
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
@@ -165,38 +130,25 @@ REST_FRAMEWORK = {
     ],
 }
 
+# === JWT CONFIG ===
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(days=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=10),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
-
-    # "ALGORITHM": "HS256",
-    # "SIGNING_KEY": settings.SECRET_KEY,
-    "VERIFYING_KEY": "",
-    "AUDIENCE": None,
-    "ISSUER": None,
-    "JSON_ENCODER": None,
-    "JWK_URL": None,
-    "LEEWAY": 0,
-
     "AUTH_HEADER_TYPES": ("Bearer",),
     "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
     "USER_ID_FIELD": "id",
     "USER_ID_CLAIM": "user_id",
     "USER_AUTHENTICATION_RULE": "rest_framework_simplejwt.authentication.default_user_authentication_rule",
-
     "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
     "TOKEN_TYPE_CLAIM": "token_type",
     "TOKEN_USER_CLASS": "rest_framework_simplejwt.models.TokenUser",
-
     "JTI_CLAIM": "jti",
-
     "SLIDING_TOKEN_REFRESH_EXP_CLAIM": "refresh_exp",
     "SLIDING_TOKEN_LIFETIME": timedelta(minutes=5),
     "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
-
     "TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainPairSerializer",
     "TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSerializer",
     "TOKEN_VERIFY_SERIALIZER": "rest_framework_simplejwt.serializers.TokenVerifySerializer",
@@ -205,60 +157,19 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
 
-# settings.py
-
-# Allow frontend to send cookies (including CSRF token)
-CSRF_TRUSTED_ORIGINS = ["http://localhost:3000", "http://192.168.1.1:3000"]  
-
-# Enable CORS for frontend
-MIDDLEWARE.insert(1, "corsheaders.middleware.CorsMiddleware")
-
-CORS_ALLOW_CREDENTIALS = True  
-CSRF_COOKIE_HTTPONLY = False  
-CSRF_COOKIE_SECURE = False
-CSRF_COOKIE_SAMESITE = "Lax"  
-
-SESSION_COOKIE_SAMESITE = "Lax"
-# CORS Config
-CORS_ALLOWED_ORIGINS = env.list('CORS_ALLOWED_ORIGINS', default=['http://localhost:3000'])
-
-
-# CRONJOBS = [
-#     ('* * * * *', 'agriBack.cron.send_data.SendDataCronJob', '>> /var/log/cron.log 2>&1'),
-#     ('* * * * *', 'agriBack.cron.send', '>> /var/log/cron2.log 2>&1'),
-# ]
-
-
-# CRONJOBS = [
-#     ('* * * * *', 'agriBack.scripts.log_time')  # Run every minute
-# ]
-
-
-
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'handlers': {
-#         'file': {
-#             'level': 'DEBUG',
-#             'class': 'logging.FileHandler',
-#             'filename': '/shared/django_cron.log',  # Specify the path where you want the log file to be stored
-#         },
-#     },
-#     'loggers': {
-#         'django': {
-#             'handlers': ['file'],
-#             'level': 'DEBUG',
-#             'propagate': True,
-#         },
-#         'django_crontab': {
-#             'handlers': ['file'],
-#             'level': 'DEBUG',
-#             'propagate': True,
-#         },
-#     },
-# }
-
+# === CRON CONFIG ===
 CRON_CLASSES = [
     "agriBack.cron.job.MyCronJob",
 ]
+
+# === EMAIL CONFIG ===
+EMAIL_HOST = os.getenv('EMAIL_HOST', '')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
+EMAIL_PORT = os.getenv('EMAIL_PORT', '')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'False') == 'True'
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'False') == 'True'
+
+# === BACKEND / FRONTEND HOST INFO ===
+B_HOST = os.getenv('B_HOST', '')
+F_HOST = os.getenv('F_HOST', '')
